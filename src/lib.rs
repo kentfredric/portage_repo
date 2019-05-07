a6aa28fa08d555834faefb246203a3bc0ad47833
Kent Fredric <kentnl@gentoo.org>
2019-05-07 19:23:44 +1200

    lib.rs: +Ebuild basics

--- src/lib.rs
+++ src/lib.rs
@@ -133,3 +133,34 @@ impl From<Package> for PathBuf {
 impl From<&Package> for PathBuf {
     fn from(other: &Package) -> Self { other.path.to_owned() }
 }
+
+/// Represent a discrete Gentoo ebuild
+#[cfg_attr(feature = "external_doc", doc(include = "struct.Ebuild.md"))]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct Ebuild {
+    root:     PathBuf,
+    category: String,
+    package:  String,
+    ebuild:   String,
+    path:     PathBuf,
+}
+
+impl Ebuild {
+    /// Construct a new ebuild explicitly
+    pub fn new<'a, P, S, SS, SSS>(
+        root: P, category: S, package: SS, ebuild: SSS,
+    ) -> Self
+    where
+        P: Into<PathBuf>,
+        S: Into<Cow<'a, str>>,
+        SS: Into<Cow<'a, str>>,
+        SSS: Into<Cow<'a, str>>,
+    {
+        let r = root.into();
+        let c = category.into().into_owned();
+        let p = package.into().into_owned();
+        let e = ebuild.into().into_owned();
+        let path = r.join(&c).join(&p).join(&e);
+        Self { root: r, category: c, package: p, ebuild: e, path }
+    }
+}
